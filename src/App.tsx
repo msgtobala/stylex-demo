7f2b9dcc9f2bf3ebc701bfdf901960ad0dca5206
Balaji Venkatraman <rslbalaji@gmail.com>
2023-12-18 01:42:32 +0530

    add stylex setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import * as stylex from '@stylexjs/stylex';
+import Button from './components/Button/Button';
 
 const styles = stylex.create({
-  red: { color: 'red', },
+  red: { color: 'red', fontSize: '36px' },
+  caption: { fontSize: '20px' },
 });
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div>
-      <h1 {...stylex.props(styles.red)}>Hai</h1>
-      <p {...stylex.props(styles.red)}>Hai</p>
+      <h1 {...stylex.props(styles.red)}>StyleX</h1>
+      <p {...stylex.props(styles.red, styles.caption)}>Specificity</p>
+      <Button>Click Me</Button>
     </div>
   );
 };
